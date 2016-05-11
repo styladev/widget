f377c94e4be34530868088efe975aa5691897946
Mouse Braun <mouse@styla.com>
2016-05-11 13:22:18 +0200

    added css replace and css wrap

--- gulpfile.js
+++ gulpfile.js
@@ -5,31 +5,36 @@ const babelify      = require( 'babelify' );
 const uglify        = require( 'gulp-uglify' );
 const header        = require( 'gulp-header' );
 const minifycss     = require( 'gulp-minify-css' );
+const cssWrap       = require( 'gulp-css-wrap' );
 const rename        = require( 'gulp-rename' );
+const replace       = require( 'gulp-replace' );
 
 const _package      = require( './package.json' );
 
 const now           = new Date();
 const year          = now.getUTCFullYear();
+const version       = _package.version;
+const homepage      = _package.homepage;
+const widgetCss     = fs.readFileSync( './dist/styles.min.css', 'utf8' );
 
 const licenceLong   = '/*!\n' +
-                      ' * Styla bite-sized widget v' + _package.version + '\n' +
-                      ' * ' + _package.homepage + '\n' +
+                      ' * Styla bite-sized widget v' + version + '\n' +
+                      ' * ' + homepage + '\n' +
                       ' *\n' +
                       ' * Copyright ' + ( 2016 === year ? year : '2016-' + year ) + ' Styla GmbH and other contributors\n' +
                       ' * Released under the MIT license\n' +
-                      ' * ' + _package.homepage + '/license.md\n' +
+                      ' * ' + homepage + '/license.md\n' +
                       ' *\n' +
                       ' * Date: ' + now.toDateString() + '\n' +
                       ' *' +
                       ' */\n';
 
-const licenceShort  = '/*! Styla Widget v' + _package.version + ' | (c) ' + ( 2016 === year ? year : '2015-' + year ) + ' Styla GmbH | ' + _package.homepage + '/license.md */\n';
+const licenceShort  = '/*! Styla Widget v' + version + ' | (c) ' + ( 2016 === year ? year : '2015-' + year ) + ' Styla GmbH | ' + homepage + '/license.md */\n';
 
 
 gulp.task( 'browserifyFiles', function()
 {
-    browserify( './src/widget.js' )
+    return browserify( './src/widget.js' )
         .transform( babelify, { stage : 0 } )
         .bundle()
         .pipe( fs.createWriteStream( __dirname + '/dist/widget.js' ) )
@@ -37,6 +42,7 @@ gulp.task( 'browserifyFiles', function()
         {
             gulp.src( './dist/widget.js' )
                 .pipe( header( licenceLong ) )
+                .pipe( replace( /styla-widget-css-goes-here/, widgetCss ) )
                 .pipe( gulp.dest( './dist/' ) )
         } );
 } );
@@ -44,7 +50,7 @@ gulp.task( 'browserifyFiles', function()
 
 gulp.task( 'min', function()
 {
-    browserify( './src/widget.js' )
+    return browserify( './src/widget.js' )
         .transform( babelify, { stage : 0 } )
         .bundle()
         .pipe( fs.createWriteStream( __dirname + '/dist/widget.min.js' ) )
@@ -53,6 +59,7 @@ gulp.task( 'min', function()
             gulp.src( './dist/widget.min.js' )
                 .pipe( uglify() )
                 .pipe( header( licenceShort ) )
+                .pipe( replace( /styla-widget-css-goes-here/, widgetCss ) )
                 .pipe( gulp.dest( './dist/' ) )
         } );
 } );
@@ -60,7 +67,7 @@ gulp.task( 'min', function()
 
 gulp.task( 'buildTests', function()
 {
-    browserify( './tests/tests.js' )
+    return browserify( './tests/tests.js' )
         .transform( babelify, { stage : 0 } )
         .bundle()
         .pipe( fs.createWriteStream( __dirname + '/tests/tests.dist.js' ) )
@@ -70,14 +77,14 @@ gulp.task( 'buildTests', function()
 gulp.task( 'css-min', function()
 {
     return gulp.src( './src/styles.css' )
-        .pipe( gulp.dest( './dist' ) )
-        .pipe( rename( { suffix: '.min' } ) )
-        .pipe( minifycss() )
-        .pipe( gulp.dest( 'dist' ) );
+                .pipe( rename( { suffix: '.min' } ) )
+                .pipe( cssWrap( { selector: '#styla-widget' } ) )
+                .pipe( minifycss() )
+                .pipe( gulp.dest( 'dist' ) );
 } );
 
 
 gulp.task( 'default', [], function()
 {
-    gulp.start( [ 'browserifyFiles', 'css-min', 'min', 'buildTests' ] );
+    gulp.start( [ 'css-min', 'browserifyFiles', 'min', 'buildTests' ] );
 } );
